5fe700815c7f646a11915c9ce02946acb1442adc
Duong <77065902+56duong@users.noreply.github.com>
2023-08-28 14:34:34 +0700

    Update Account.java

--- src/beatalbumshop/config/Account.java
+++ src/beatalbumshop/config/Account.java
@@ -7,15 +7,15 @@ public class Account {
     /**
      * The Firebase project ID for the Beat Album Shop.
      */
-    public static final String FIREBASE_PROJECT_ID = "beat-75a88";
+    public static final String FIREBASE_PROJECT_ID = "YOUR_FIREBASE_PROJECT_ID"; // Update this with your actual Firebase Project ID
     
     /**
      * The Spotify client ID for authentication with the Spotify API.
      */
-    public static final String SPOTIFY_CLIENT_ID = "fd38e4ef55d34fde98be48fba49fac22";
+    public static final String SPOTIFY_CLIENT_ID = "YOUR_SPOTIFY_CLIENT_ID"; // Update this with your actual Spotify Client ID
     
     /**
      * The Spotify client secret for authentication with the Spotify API.
      */
-    public static final String SPOTIFY_CLIENT_SECRET = "52c18dfc990a48a08c8bc11a76b9cfbb";
+    public static final String SPOTIFY_CLIENT_SECRET = "YOUR_SPOTIFY_CLIENT_SECRET"; // Update this with your actual Spotify Client Secret
 }
